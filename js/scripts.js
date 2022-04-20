399b009823cb1f7e53d6cef9139845c06dc28b1c
Kenny Ngo <kngo@umich.edu>
2022-04-20 08:39:18 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+/*!
+ * Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+ */
+//
+// Scripts
+//
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Activate Bootstrap scrollspy on the main nav element
+  const sideNav = document.body.querySelector("#sideNav");
+  if (sideNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#sideNav",
+      offset: 74,
+    });
+  }
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
+
+  // // Add smooth scrolling to all links
+  // $("a").on("click", function (event) {
+  //   // Make sure this.hash has a value before overriding default behavior
+  //   if (this.hash !== "") {
+  //     // Prevent default anchor click behavior
+  //     event.preventDefault();
+
+  //     // Store hash
+  //     var hash = this.hash;
+
+  //     // Using jQuery's animate() method to add smooth page scroll
+  //     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+  //     $("html, body").animate(
+  //       {
+  //         scrollTop: $(hash).offset().top,
+  //       },
+  //       850,
+  //       function () {
+  //         // Add hash (#) to URL when done scrolling (default click behavior)
+  //         window.location.hash = hash;
+  //       }
+  //     );
+  //   } // End if
+  // });
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var hash = this.hash;
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          850,
+          "easeInOutExpo",
+          function () {
+            window.location.hash = hash;
+          }
+        );
+        return false;
+      }
+    }
+  });
+});
