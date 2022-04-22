ee50116f44ce56f150acb0e6bd53ba37dbf84762
kngo <kngo@umich.edu>
2022-04-22 00:42:06 -0400

    update styles.css, index.html and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 // import Typed from 'typed.js';
 // const Typed = require('Typed.js');
 
-
 /*!
  * Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
  * Copyright 2013-2022 Start Bootstrap
@@ -33,31 +32,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
       }
     });
   });
-
-  // // Add smooth scrolling to all links
-  // $("a").on("click", function (event) {
-  //   // Make sure this.hash has a value before overriding default behavior
-  //   if (this.hash !== "") {
-  //     // Prevent default anchor click behavior
-  //     event.preventDefault();
-
-  //     // Store hash
-  //     var hash = this.hash;
-
-  //     // Using jQuery's animate() method to add smooth page scroll
-  //     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-  //     $("html, body").animate(
-  //       {
-  //         scrollTop: $(hash).offset().top,
-  //       },
-  //       850,
-  //       function () {
-  //         // Add hash (#) to URL when done scrolling (default click behavior)
-  //         window.location.hash = hash;
-  //       }
-  //     );
-  //   } // End if
-  // });
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
